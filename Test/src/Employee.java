76af9ee90bbc63a6cb2d3ae7718df1ac07a99a73
xirenbunong <m18791744718@163.com>
2018-07-22 22:36:20 +0800

    extends

---
+++ Test/src/Employee.java
@@ -0,0 +1,12 @@
+/**
+ * Created by li on 2018/7/22.
+ */
+public class Employee {
+    private String name;
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+}
---
+++ Test/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Employee employee = new Employee();
+        employee.setName("xiren");
+        System.out.println("员工姓名：" + employee.getName());
+    }
+}
